88b9ae138c87f6f111c9ccac142a8d2f3e7a9e72
Lord-Awesome <cknebel@umich.edu>
2017-06-03 14:49:32 -0600

    Add files via upload

--- hue_test.py
+++ hue_test.py
@@ -1,6 +1,7 @@
 import Adafruit_WS2801
 import Adafrit_GPIO.SPI as SPI
 import time
+import random
 PIXEL_COUNT = 160
 PIXEL_CLOCK = 11
 PIXEL_DOUT  = 10
@@ -66,4 +67,34 @@ for i in range(len(color_list)):
     pixels.show()
     if wait > 0:
         time.sleep(wait)
+
+pixels.clear()
+time.sleep(2)
+pixels_list = list(range(pixels.count()))
+random.shuffle(pixels_list)
+
+# Same as above, but the LEDS don't display sequential colors. The same colors display over the lights, but the lights are shuffled.
+for i in range(len(color_list)):
+    for j in pixels_list:
+        k = (i+j)%len(color_list) #if we exceed the list, wrap around
+        pixels.set_pixel(j, color_list[k])
+    pixels.show()
+    if wait > 0:
+        time.sleep(wait)
+
+pixels.clear()
+time.sleep(2)
+pixels_list = list(range(pixels.count()))
+random.shuffle(pixels_list)
+random.shuffle(color_list)
+
+# Same as above, but both the color list and the LED order are shuffled.
+for i in range(len(color_list)):
+    for j in pixels_list:
+        k = (i+j)%len(color_list) #if we exceed the list, wrap around
+        pixels.set_pixel(j, color_list[k])
+    pixels.show()
+    if wait > 0:
+        time.sleep(wait)
+
     
